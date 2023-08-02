1e5cc4009a3c2a7b1c3764c1b337b346cf09c910
xinjalin <xinjalin@gmail.com>
2023-08-03 02:40:54 +0930

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
+// Imports
 require('dotenv').config();
-
 const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
-
 const cookieParser = require('cookie-parser');
-
 const PORT = process.env.PORT || 3500;
 
+// Express Application Setup
 app.use(cors(corsOptions));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
@@ -20,6 +19,7 @@ app.use(cookieParser());
 app.use('/', require('./routes/root'));
 app.use('/auth', require('./routes/auth'));
 
+// All undefined endpoints return a 404 html page
 app.all('*', (req, res) => {
     res.status(404);
     if (req.accepts('html')) {
