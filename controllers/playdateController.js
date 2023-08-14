b8fe62d7c511373cbfc95f0a602c9feaeb6cb3ee
xinjalin <xinjalin@gmail.com>
2023-08-14 17:43:12 +0930

    es lint

--- controllers/playdateController.js
+++ controllers/playdateController.js
@@ -1,8 +1,8 @@
+/* eslint-disable camelcase */
 const sql = require('mssql');
 const config = require('../config/dbconfig');
 
 const handlePlayDate = async (req, res) => {
-
     const {sender_user_id, receiver_user_id, play_date, play_time} = req.body;
     if (!sender_user_id || !receiver_user_id || !play_date || !play_time) return res.status(400).json({'message': 'Invalid playdate request'});
 
@@ -12,9 +12,9 @@ const handlePlayDate = async (req, res) => {
         .input('sql_receiver_user_id', sql.Int, receiver_user_id)
         .input('sql_play_date', sql.Date, play_date)
         .input('sql_play_time', sql.NVarChar, play_time)
-        .query('INSERT INTO vetdata.dim_playdates VALUES (@sql_sender_user_id, @sql_receiver_user_id, @sql_play_date, @sql_play_time)')
+        .query('INSERT INTO vetdata.dim_playdates VALUES (@sql_sender_user_id, @sql_receiver_user_id, @sql_play_date, @sql_play_time)');
 
     return res.sendStatus(200);
-}
+};
 
 module.exports = {handlePlayDate};
