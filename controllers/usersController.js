29bbe52f7f95c8a004fde6bed9679b887f07a0ad
DarthOhmost <123339632+DarthOhmost@users.noreply.github.com>
2023-09-19 15:15:23 +0930

    Added Get All User's Info

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -13,4 +13,23 @@ const handleUsers = async (req, res) => {
     }
 }
 
-module.exports = {handleUsers};
+const handleUsersInfo = async (req, res) => {
+    if (!req?.params?.userId) return res.status(400).json({ 'message': 'Employee User ID required.' });
+    
+    const userId = req?.params?.userId;
+    //console.log(userId) debugging
+    try {
+        const pool = await sql.connect(config);
+        const response = await pool.request()
+            .input('sql_user_id', sql.Int, userId)
+            .query('SELECT * FROM vetdata.users WHERE user_id = @sql_user_id');
+        return res.status(200).json(...response.recordsets);
+    } catch (err) {
+        return res.status(500).json({'message': err.message });
+    }
+}
+
+module.exports = {
+    handleUsers,
+    handleUsersInfo
+};
